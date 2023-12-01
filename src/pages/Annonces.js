42be29e45fbbc9f6a6390458d27b3a32b05224ce
BANNAFOO <rithus.bannafoo@gmail.com>
2023-12-01 07:43:16 -0300

    Header terminé

--- src/pages/Annonces.js
+++ src/pages/Annonces.js
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
-
+// import imgcode from '../image/imgcode.jpg';
 function Annonces() {
     return(
         <header> 
           <Header/>
           <h1>Annonces</h1>
+
         </header>
        
         );
